4e9ffd767eb2c29c790af5dcd0508389046f8f90
Santiago Alé <ale.sj.dev@gmail.com>
2025-12-18 16:43:36 -0300

    chore: limpiar rutas y páginas innecesarias

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,5 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
-
-Route::get('dashboard', function () {
     return Inertia::render('dashboard');
 })->name('dashboard');
